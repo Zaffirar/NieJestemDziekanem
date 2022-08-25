f28d33e6aa97cfb4d76613176f0e12ae3ab9fbbb
Paweł Głowacki <45500823+PawixXD@users.noreply.github.com>
2022-08-26 01:34:42 +0200

    Update new_types.py

--- new_types.py
+++ new_types.py
@@ -21,8 +21,8 @@ class QuestionsSet:
     _questions: List[Question]
 
     # TODO: establish how many questions do we need
-    def __init__(self, Q1: Question, Q2: Question, Q3: Question, Q4: Question, Q5: Question) -> None:
-        self._questions = [Q1, Q2, Q3, Q4, Q5]
+    def __init__(self, questions) -> None:
+        self._questions = questions
 
     def to_dict(self) -> Dict[str, PossibleAnswers]:
         _dict: Dict[str, PossibleAnswers] = {}
